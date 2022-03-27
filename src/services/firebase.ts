ae6f30b4e4e8b0e6d9d3588bd98126bba2b6b6e4
André Angeloni <andrefangeloni@gmail.com>
2022-03-27 19:23:26 -0300

    feat: environment setup and firebase config

---
+++ src/services/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app';
+
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MSG_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+initializeApp(firebaseConfig);
+
+export const auth = getAuth();
+export const database = getDatabase();
